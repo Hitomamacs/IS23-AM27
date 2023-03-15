1fd8dce993113f06e9d4803d8ae181b031b3249e
Hitomamacs <macsdeve@gmail.com>
2023-03-15 11:33:53 +0100

    added attributes game board

--- GameCode/src/main/java/org/example/GameBoard.java
+++ GameCode/src/main/java/org/example/GameBoard.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class GameBoard {
+    private Spot[][] board;
+    private int[][] negativeMatrix;
+    private int[][] oneMatrix;
+    private int[][] fiveMatrix;;
 }

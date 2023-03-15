754457f3837ff03d4dd227a3302e430764d9312c
AleGB29 <alessandrogwyn.barbieri@mail.polimi.it>
2023-03-15 12:55:38 +0100

    implementation of attributes setters and getters player class

--- GameCode/src/main/java/org/example/Player.java
+++ GameCode/src/main/java/org/example/Player.java
@@ -1,4 +1,53 @@
 package org.example;
 
+
+import java.util.Set;
+
 public class Player {
+    private String nickname;
+   private boolean isConnected;
+   private int score;
+   private PlayerGrid playerGrid;
+   private boolean[] CompletedCGoals;
+   //TODO  we still need to create the class PersonalGoal so right now this would give an error private PersonalGoal myPersonalGoal;
+   private Set<Tile> pickedTiles;
+   
+    public Player() {
+    }
+    public String getNickname() {
+        return nickname;
+    }
+    public int getScore() {
+        return score;
+    }
+    public boolean isConnected() {
+        return isConnected;
+    }
+    public PlayerGrid getPlayerGrid() {
+        return playerGrid;
+    }
+    public boolean[] getCompletedCGoals() {
+        return CompletedCGoals;
+    }
+    public Set<Tile> getPickedTiles() {
+        return pickedTiles;
+    }
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+    public void setConnected(boolean connected) {
+        isConnected = connected;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
+    public void setPlayerGrid(PlayerGrid playerGrid) {
+        this.playerGrid = playerGrid;
+    }
+    public void setCompletedCGoals(boolean[] completedCGoals) {
+        CompletedCGoals = completedCGoals;
+    }
+    public void setPickedTiles(Set<Tile> pickedTiles) {
+        this.pickedTiles = pickedTiles;
+    }
 }

d6126ddaac8989a7500987398fd4017fb3869d79
Silvia <silvia.denti@mail.polimi.it>
2023-03-16 18:26:00 +0100

    added all PersonalGoal classes and testing

--- GameCode/src/main/java/org/example/PersonalGoal_7.java
+++ GameCode/src/main/java/org/example/PersonalGoal_7.java
@@ -1,37 +1,21 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class PersonalGoal_7 extends PersonalGoal{
-    private final Spot[][] spotGrid;
+    private HashMap<Coordinates, Color> coloredGoal;
 
-    public PersonalGoal_7() {
-        spotGrid = new Spot[6][5];
-        initializeGoalGrid();
+    public void initialize() {
+        coloredGoal = new HashMap<>();
+        coloredGoal.put(new Coordinates(2, 1), Color.PINK);
+        coloredGoal.put(new Coordinates(1, 3), Color.BLUE);
+        coloredGoal.put(new Coordinates(0, 0), Color.GREEN);
+        coloredGoal.put(new Coordinates(5, 2), Color.WHITE);
+        coloredGoal.put(new Coordinates(4, 4), Color.YELLOW);
+        coloredGoal.put(new Coordinates(3, 0), Color.AZURE);
     }
 
-    //grid initialization with all spots not occupied
-    public void initializeGoalGrid() {
-        //Spot matrix initialization
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 5; j++) {
-                spotGrid[i][j] = new Spot(false, null);
-            }
-        }
-
-        spotGrid[0][0].placeTile(new Tile(Color.GREEN, 0));
-        spotGrid[0][0].setOccupied(true);
-        spotGrid[1][3].placeTile(new Tile(Color.BLUE, 0));
-        spotGrid[1][3].setOccupied(true);
-        spotGrid[4][4].placeTile(new Tile(Color.YELLOW,0));
-        spotGrid[4][4].setOccupied(true);
-        spotGrid[3][0].placeTile(new Tile(Color.AZURE,0));
-        spotGrid[3][0].setOccupied(true);
-        spotGrid[5][2].placeTile(new Tile(Color.WHITE,0));
-        spotGrid[5][2].setOccupied(true);
-        spotGrid[2][1].placeTile(new Tile(Color.PINK, 0));
-        spotGrid[2][1].setOccupied(true);
-    }
-
-    public Spot[][] getSpotGrid() {
-        return spotGrid;
+    public HashMap<Coordinates, Color> getColoredGoal() {
+        return coloredGoal;
     }
 }
--- GameCode/src/test/java/org/example/PersonalGoal_6Test.java
+++ GameCode/src/test/java/org/example/PersonalGoal_6Test.java
@@ -3,60 +3,44 @@ package org.example;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonalGoal_6Test {
+    PersonalGoal_6 personalGoal_6;
+    private final Coordinates a = new Coordinates(5,0);
+    private final Coordinates b = new Coordinates(4,3);
+    private final Coordinates c = new Coordinates(0,4);
+    private final Coordinates d = new Coordinates(2,3);
+    private final Coordinates e = new Coordinates(4,1);
+    private final Coordinates f = new Coordinates(0,2);
 
-    PersonalGoal_6 goal = new PersonalGoal_6();
-    Spot[][] spots = goal.getSpotGrid();
 
     @BeforeEach
     void setUp(){
-        goal = new PersonalGoal_6();
+        personalGoal_6 = new PersonalGoal_6();
+
     }
 
-    //Test 1: check that the tiles are in the right position
     @Test
-    void checkCorrectPosition(){
-        assert spots[0][4].getTile().getColor().equals(Color.GREEN);
-        assert spots[5][0].getTile().getColor().equals(Color.PINK);
-        assert spots[4][1].getTile().getColor().equals(Color.YELLOW);
-        assert spots[0][2].getTile().getColor().equals(Color.AZURE);
-        assert spots[2][3].getTile().getColor().equals(Color.WHITE);
-        assert spots[4][3].getTile().getColor().equals(Color.BLUE);
+    public void testInitialize() {
+
+        personalGoal_6.initialize();
+        HashMap<Coordinates, Color> coloredGoal = personalGoal_6.getColoredGoal();
+        assertEquals(6, coloredGoal.size());
+        assertTrue(coloredGoal.containsKey(a));
+        assertTrue(coloredGoal.containsKey(b));
+        assertTrue(coloredGoal.containsKey(c));
+        assertTrue(coloredGoal.containsKey(d));
+        assertTrue(coloredGoal.containsKey(e));
+        assertTrue(coloredGoal.containsKey(f));
+        assertEquals(Color.PINK, coloredGoal.get(a));
+        assertEquals(Color.BLUE, coloredGoal.get(b));
+        assertEquals(Color.GREEN, coloredGoal.get(c));
+        assertEquals(Color.WHITE, coloredGoal.get(d));
+        assertEquals(Color.YELLOW, coloredGoal.get(e));
+        assertEquals(Color.AZURE, coloredGoal.get(f));
     }
 
-    //Test 2: check that other spots are empty
-    @Test
-    void emptyCheckPosition(){
-        for(int i=0; i<6; i++){
-            for(int j=0; j<5; j++){
-                if((i==0 && j==4) || (i==5 && j==0) || (i==4 && j==1) || (i==0 && j==2) || (i==2 && j==3)
-                        || (i==4 && j==3)){
-                    continue;
-                }
-                assert !spots[i][j].isOccupied();
-            }
-        }
-    }
-
-    //Test 3: check that the filled Spots have the attribute occupied=true
-    @Test
-    void fullCheckPosition(){
-        for(int i=0; i<6; i++){
-            for(int j=0; j<5; j++){
-                if((i==0 && j==4) || (i==5 && j==0) || (i==4 && j==1) || (i==0 && j==2) || (i==2 && j==3)
-                        || (i==4 && j==3)){
-                    assert spots[i][j].isOccupied();
-                }
-            }
-        }
-    }
-
-    //Test 4: check that spots matrix is created correctly
-    @Test
-    void checkCorrectMatrix(){
-        assertEquals(6, spots.length);
-        assertEquals(5, spots[0].length);
-    }
 }

e816a0b05cac6280e126e2c0cf0e89a549799251
AleGB29 <alessandrogwyn.barbieri@mail.polimi.it>
2023-03-17 00:53:30 +0100

    verifyPGoalPoints method and beginning of its test class

--- GameCode/src/main/java/org/example/Player.java
+++ GameCode/src/main/java/org/example/Player.java
@@ -1,7 +1,9 @@
 package org.example;
 
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Player {
     private String nickname;
@@ -9,9 +11,8 @@ public class Player {
    private int score;
    private PlayerGrid playerGrid;
    private boolean[] CompletedCGoals;
-   //TODO  we still need to create the class PersonalGoal so right now this would give an error private PersonalGoal myPersonalGoal;
+   private PersonalGoal myPersonalGoal;
    private List<Tile> pickedTiles;
-
     public Player() {
         CompletedCGoals = new boolean[2];
         playerGrid = new PlayerGrid();
@@ -31,6 +32,9 @@ public class Player {
     public boolean[] getCompletedCGoals() {
         return CompletedCGoals;
     }
+    public PersonalGoal getMyPersonalGoal() {
+        return myPersonalGoal;
+    }
     public List<Tile> getPickedTiles() {
         return pickedTiles;
     }
@@ -49,21 +53,66 @@ public class Player {
     public void setCompletedCGoals(boolean[] completedCGoals) {
         CompletedCGoals = completedCGoals;
     }
+    public void setMyPersonalGoal(PersonalGoal personalGoal){
+        myPersonalGoal = personalGoal;
+    }
     public void setPickedTiles(List<Tile> pickedTiles) {
         this.pickedTiles = pickedTiles;
     }
-
-    //Change score is the method used to increment the score in player. Score can only increase so if a negative
+    //changeScore is the method used to increment the score in player. Score can only increase so if a negative
     //int is passed it throws an exception
     public void changeScore(int points){
         if (points < 0)
             throw new IllegalArgumentException("Points can only be added");
         score = score + points;
     }
+    //modifyCompletedCGoals is a method that allows me to change the boolean that indicates if a player
+    //has already accomplished a certain common goal
     public void modifyCompletedCGoals(int position){
-        if(position  >= 2 || position < 0)
-            throw new IllegalArgumentException("Position must be part of {0,1}");  //alternatively if(position >= CompletedCGoals.lenght)
-        CompletedCGoals[position] = ! CompletedCGoals[position];
+        if(position < 0 || position > 1)
+            throw new IllegalArgumentException("Position must be in {0,1}");
+        CompletedCGoals[position] = !CompletedCGoals[position];
     }
+    //verifyPGoalPoints returns the points that the player receives from his personal goal
+    public int verifyPGoalPoints(){
+
+        HashMap<Coordinates,Color> colorMap;
+        Tile tile;
+        Coordinates coordinate;
+        Color color;
+        int count = 0;
+        colorMap = myPersonalGoal.getColoredGoal();
+        //for loop iterates on hash map and checks for each coordinate if there is a tile and if the color is correct
+        //in which case count is incremented
+        for (Map.Entry<Coordinates, Color> mapElement : colorMap.entrySet()){
+            coordinate = mapElement.getKey();
+            color = mapElement.getValue();
+            if(playerGrid.getSpot(coordinate).isOccupied()){
+                tile = playerGrid.getSpot(coordinate).getTile();
+                if(tile.getColor() == color){
+                    count++;
+                }
+            }
+        }
+        switch(count){  //switch to determine how many points are assigned based on number of correct tiles
+            case 1:
+                return 1;
+            case 2:
+                return 2;
+            case 3:
+                return 4;
+            case 4:
+                return 6;
+            case 5:
+                return 9;
+            case 6:
+                return 12;
+        }
+    }
+
+    //verifyExtraPoints is the method that checks how many points to assign for groups of same colored tiles
+    //in playergrid adiacent to each other.
+    //public int verifyExtraPoints()
+
 }
 
--- GameCode/src/test/java/org/example/PlayerTest.java
+++ GameCode/src/test/java/org/example/PlayerTest.java
@@ -23,29 +23,39 @@ class PlayerTest {
         player.changeScore(0);
         assertEquals(player.getScore(), 5);
         //TODO test for exception missing if I pass negative int
-
     }
     @Test
     void testModifyCompletedCGoals(){
-        boolean[] CGoals;
-        CGoals = player.getCompletedCGoals();
-        assertFalse(CGoals[0]);
-        assertFalse(CGoals[1]);
-        player.modifyCompletedCGoals(0);
-        CGoals = player.getCompletedCGoals();
-        assertTrue(CGoals[0]);
-        assertFalse(CGoals[1]);
-        player.modifyCompletedCGoals(1);
-        CGoals = player.getCompletedCGoals();
-        assertTrue(CGoals[0]);
-        assertTrue(CGoals[1]);
-        player.modifyCompletedCGoals(0);
-        player.modifyCompletedCGoals(1);
-        CGoals = player.getCompletedCGoals();
-        assertFalse(CGoals[0]);
-        assertFalse(CGoals[1]);
-        //TODO missing exception test if I pass position not in {1,2}
+        boolean[] test;
+         test = player.getCompletedCGoals();
+         assertFalse(test[0]);
+         assertFalse(test[1]);
+         player.modifyCompletedCGoals(0);
+         test = player.getCompletedCGoals();//makes no sense now as getter is returning a direct reference but in case
+                                            //getter is changed to return a copy
+         assertTrue(test[0]);
+         assertFalse(test[1]);
+         player.modifyCompletedCGoals(1);
+         test = player.getCompletedCGoals();
+         assertTrue(test[0]);
+         assertTrue(test[1]);
+         //TODO test exception in case position is not in {0,1}
+
     }
+    @Test
+    void testVerifyPGoalsPoints(){
+        PersonalGoal PGoal;
+        PGoal = new PersonalGoal_1;
+        Tile tile;
+        player.setMyPersonalGoal(PGoal);
+
+        assertEquals(player.verifyPGoalPoints(), 0);
+
+        tile = new Tile(Color.PINK, 0);
+        player.getPlayerGrid().topUp(0,tile);
+    //TODO finish test cases
+    }
+
 
 
 }

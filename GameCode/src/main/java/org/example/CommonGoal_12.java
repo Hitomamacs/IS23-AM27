28b58b7c0df47347d2e3528044a07553151846fe
Silvia <silvia.denti@mail.polimi.it>
2023-03-17 16:33:03 +0100

    added common goal 12

--- GameCode/src/main/java/org/example/CommonGoal_12.java
+++ GameCode/src/main/java/org/example/CommonGoal_12.java
@@ -1,4 +1,18 @@
 package org.example;
 
 public class CommonGoal_12 extends CommonGoal{
+
+    public boolean checkGoal(PlayerGrid playerGrid){
+        //counter for the matrix
+        int i=0;
+        int j=0;
+
+        for(i=0; i<5; i++){
+            for(j=0; j<6; j++){
+
+            }
+        }
+
+        if(playerGrid)
+    }
 }

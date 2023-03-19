1f287d1948536e676de421a9d22d217374156602
Silvia <silvia.denti@mail.polimi.it>
2023-03-19 15:02:24 +0100

    common classes

--- GameCode/src/main/java/org/example/CommonGoal_8.java
+++ GameCode/src/main/java/org/example/CommonGoal_8.java
@@ -5,15 +5,15 @@ public class CommonGoal_8 extends CommonGoal{
     public boolean checkGoal(PlayerGrid playerGrid){
 
         if(playerGrid.getSpot(new Coordinates(0, 0)).isOccupied() &&
-                playerGrid.getSpot(new Coordinates(4, 0)).isOccupied() &&
-                playerGrid.getSpot(new Coordinates(0,5)).isOccupied() &&
-                playerGrid.getSpot(new Coordinates(4, 5)).isOccupied() &&
+                playerGrid.getSpot(new Coordinates(0, 4)).isOccupied() &&
+                playerGrid.getSpot(new Coordinates(5,0)).isOccupied() &&
+                playerGrid.getSpot(new Coordinates(5, 4)).isOccupied() &&
                 (playerGrid.getSpot(new Coordinates(0,0)).getTile().getColor() ==
-                        playerGrid.getSpot(new Coordinates(4,0)).getTile().getColor()) &&
+                        playerGrid.getSpot(new Coordinates(0,4)).getTile().getColor()) &&
                 (playerGrid.getSpot(new Coordinates(0,0)).getTile().getColor() ==
-                        playerGrid.getSpot(new Coordinates(0,5)).getTile().getColor()) &&
+                        playerGrid.getSpot(new Coordinates(5,0)).getTile().getColor()) &&
                 (playerGrid.getSpot(new Coordinates(0,0)).getTile().getColor() ==
-                        playerGrid.getSpot(new Coordinates(4,5)).getTile().getColor())) {
+                        playerGrid.getSpot(new Coordinates(5,4)).getTile().getColor())) {
             return true;
         }
         return false;

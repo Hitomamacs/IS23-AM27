c5d37e5409cae448bf814193f6b338f482324110
Silvia <silvia.denti@mail.polimi.it>
2023-03-20 09:18:29 +0100

    implementig my commongoals

--- GameCode/src/main/java/org/example/CommonGoal_12.java
+++ GameCode/src/main/java/org/example/CommonGoal_12.java
@@ -8,31 +8,50 @@ public class CommonGoal_12 extends CommonGoal{
         int j=0;
         int counter_1=0, counter_2=0, counter_3=0, counter_4=0;
 
-        for(i=0; i<5; i++){
-            for(j=0; j<i; j++){
-                if(playerGrid.getSpot(new Coordinates(i, j)).isOccupied() &&
-                        playerGrid.getSpot(new Coordinates(i + 1, j + 1)).isOccupied()){
+        for(i = 0; i < 5; i++){
+            for(j = 0; j <= i; j++){
+                if(playerGrid.getSpot(new Coordinates(i, j)).isOccupied()){
                     counter_1++;
+                    if(counter_1==15){
+                        return true;
+                    }
                 }
             }
-        }
-        if(counter_1==15){
-            return true;
         }
 
-        for(i=1; i<5; i++){
-            for(j=1; j<6; j++){
-                if(playerGrid.getSpot(new Coordinates(i, j)).isOccupied() &&
-                        playerGrid.getSpot(new Coordinates(i - 1, j - 1)).isOccupied() &&
-                        playerGrid.getSpot(new Coordinates(i - 1, j - 1)).getTile().getColor() ==
-                                playerGrid.getSpot(new Coordinates(i, j)).getTile().getColor()){
+        for(i = 1; i < 6; i++){
+            for(j = 0; j <= i; j++){
+                if(playerGrid.getSpot(new Coordinates(i, j)).isOccupied()){
                     counter_2++;
+                    if(counter_2==15){
+                        return true;
+                    }
                 }
             }
         }
-        if(counter_2==15){
-            return true;
+
+        for(i = 0; i < 5; i++){
+            for(j = 4; j >= i; j--){
+                if(playerGrid.getSpot(new Coordinates(i, j)).isOccupied()){
+                    counter_3++;
+                    if(counter_3==15){
+                        return true;
+                    }
+                }
+            }
         }
+
+        for(i = 1; i < 6; i++){
+            for(j = 4; j >= i; j--){
+                if(playerGrid.getSpot(new Coordinates(i, j)).isOccupied()){
+                    counter_4++;
+                    if(counter_4==15){
+                        return true;
+                    }
+                }
+            }
+        }
+
         return false;
     }
 }
--- GameCode/src/main/java/org/example/CommonGoal_3.java
+++ GameCode/src/main/java/org/example/CommonGoal_3.java
@@ -3,6 +3,9 @@ package org.example;
 public class CommonGoal_3 extends CommonGoal{
 
     public boolean checkGoal(PlayerGrid playerGrid) {
+
+
+
         return false;
     }
 }

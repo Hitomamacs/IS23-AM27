50f07517719016d2389bc30bd50788d01ab4a5c3
AleGB29 <alessandrogwyn.barbieri@mail.polimi.it>
2023-03-20 15:52:24 +0100

    common goal 1 and 2 class and checkGoal method

--- GameCode/src/test/java/org/example/CommonGoal_1Test.java
+++ GameCode/src/test/java/org/example/CommonGoal_1Test.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,5 +14,20 @@ class CommonGoal_1Test {
         commonGoal = new CommonGoal_1();
     }
 
+    @Test
+    boolean test1_CheckGoal(){
+        Color[][] matrix;
+        boolean result = false;
+
+        matrix = new Color[][]{{Color.PINK, Color.AZURE, Color.GREEN, Color.WHITE, Color.PINK},
+                {Color.PINK, Color.PINK,Color.WHITE,Color.AZURE, Color.GREEN},
+                {Color.GREEN, Color.GREEN, Color.AZURE, Color.YELLOW, Color.WHITE},
+                {Color.PINK, Color.AZURE, Color.GREEN, Color.WHITE, Color.PINK},
+                {Color.PINK, Color.AZURE, Color.AZURE, Color.GREEN, Color.WHITE},
+                {Color.PINK, Color.AZURE, Color.GREEN, Color.WHITE, Color.PINK}};
+        result = commonGoal.checkGoal(playerGrid);
+        playerGrid.printColorPlayerGrid();
+        return result;
+    }
 
 }

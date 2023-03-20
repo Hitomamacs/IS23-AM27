5d1589d6715dbf1cac2f7e9bf7aae3b0124bf72f
mary <mariarosaria.cotrone@mail.polimi.it>
2023-03-20 10:37:22 +0100

    reimplementation goal 10 and new implementation goal 4

--- GameCode/src/main/java/org/example/CommonGoal_10.java
+++ GameCode/src/main/java/org/example/CommonGoal_10.java
@@ -1,9 +1,21 @@
 package org.example;
-
-    public boolean checkGoal(PlayerGrid playerGrid){
 public class CommonGoal_10 extends CommonGoal{
 
     public boolean checkGoal(PlayerGrid playerGrid) {
+        int i,j;
+        int color;
+
+        for(i=1;i<4;i++){
+            for(j=1;j<5;j++){
+                color=playerGrid.getSpot(new Coordinates(i,j)).getTile().getColor().ordinal();
+                if(playerGrid.getSpot(new Coordinates(i-1,j-1)).getTile().getColor().ordinal()==color &&
+                    playerGrid.getSpot(new Coordinates(i-1,j+1)).getTile().getColor().ordinal()==color &&
+                    playerGrid.getSpot(new Coordinates(i+1,j-1)).getTile().getColor().ordinal()==color &&
+                    playerGrid.getSpot(new Coordinates(i+1,j+1)).getTile().getColor().ordinal()==color){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }

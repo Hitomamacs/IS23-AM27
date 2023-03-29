373f6d82758cf4a4d9b9a712dd4513e0e1849a52
AleGB29 <alessandrogwyn.barbieri@mail.polimi.it>
2023-03-30 00:53:05 +0200

    start Turn test implementation

--- GameCode/src/test/java/org/example/StartTurnStateTest.java
+++ GameCode/src/test/java/org/example/StartTurnStateTest.java
@@ -21,13 +21,28 @@ class StartTurnStateTest {
             orchestrator.getCurrentPlayer().setConnected(true);
             orchestrator.nextPlayer();
         }
-        orchestrator.nextPlayer();
     }
+    //In this first test we have that the player is connected and his pickedTiles are empty as the game has
+    //just started. So the next state will be the verifygrillable state and the board will have been refilled
     @Test
     void goesToRefill(){
         orchestrator.executeState();
         assertTrue(orchestrator.getState() instanceof VerifyGrillableState);
         orchestrator.getGameBoard().printBoardColor();
+        orchestrator.getGameBoard().printMwithTiles();
+        assertEquals(0, orchestrator.CurrentPlayerIndex());
     }
+    //In this other case we check what happens if the player is signaled as disconnected, The next state has
+    //to be startTurn but of the following player
+     @Test
+    void goesToNextStartTurn(){
+        orchestrator.getCurrentPlayer().setConnected(false);
+        orchestrator.executeState();
+        assertTrue(orchestrator.getState() instanceof VerifyGrillableState);
+        orchestrator.getGameBoard().printBoardColor();
+        orchestrator.getGameBoard().printMwithTiles();
+         assertEquals(1, orchestrator.CurrentPlayerIndex());
+
+     }
 
 }

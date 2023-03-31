2b67a4251c137b3ebf864b4c0ae49dedbea70f3f
Silvia <silvia.denti@mail.polimi.it>
2023-03-31 17:02:50 +0200

    tests tileBag

--- GameCode/src/test/java/org/example/TileBagTest.java
+++ GameCode/src/test/java/org/example/TileBagTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +16,18 @@ class TileBagTest {
     void initializeBag() {
         TileBag tilebag=new TileBag();
         assertEquals(132, tilebag.getBag().size());
+    }
+
+    @Test
+    public void testCheckCorrectNumberColor(){
+        TileBag tileBag = new TileBag();
+
+        for(Color color : Color.values()){
+            Set<Tile> tiles = tileBag.getBag().stream()
+                    .filter(tile -> tile.getColor() == color)
+                    .collect(Collectors.toSet());
+            assertEquals(22, tiles.size());
+        }
     }
 
     //test to see that I take the right number of tiles and that they are no longer present in tilebags

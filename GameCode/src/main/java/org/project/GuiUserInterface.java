8b6aa46588df761d58bd61046f2748d08bddf0a6
mary <mariarosaria.cotrone@mail.polimi.it>
2023-05-22 19:03:47 +0200

    odio il pom ma l'ho fatto

---
+++ GameCode/src/main/java/org/project/GuiUserInterface.java
@@ -0,0 +1,11 @@
+package org.project;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GuiUserInterface extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}

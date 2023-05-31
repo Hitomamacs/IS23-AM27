e2cd03e1f6cda3d3f3e75ccd281336898048e6b6
mary <mariarosaria.cotrone@mail.polimi.it>
2023-05-31 15:35:04 +0200

    module info

---
+++ GameCode/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module AM27 {
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires java.rmi;
+    requires java.datatransfer;
+    requires java.desktop;
+    requires java.sql;
+    requires com.google.gson;
+
+    opens org.project.Gui to javafx.fxml, javafx.controls, javafx.graphics;
+    exports org.project.Gui;
+
+}

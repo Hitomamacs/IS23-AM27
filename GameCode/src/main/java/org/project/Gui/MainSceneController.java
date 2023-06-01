0a95d2641c45cfe57fa9bf0778ed951e2f6dc0ac
mary <mariarosaria.cotrone@mail.polimi.it>
2023-06-01 22:41:35 +0200

    main scene controller

---
+++ GameCode/src/main/java/org/project/Gui/MainSceneController.java
@@ -0,0 +1,9 @@
+package org.project.Gui;
+
+public class MainSceneController {
+    private GuiUserInterface guiUserInterface;
+
+    public void setGuiUserInterface(GuiUserInterface guiUserInterface) {
+        this.guiUserInterface = guiUserInterface;
+    }
+}

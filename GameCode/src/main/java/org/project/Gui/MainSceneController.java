c30731f9549af1fd346b15956b49549c6f41ee27
AleGB29 <alessandrogwyn.barbieri@mail.polimi.it>
2023-06-19 11:45:50 +0200

    pick starting

--- GameCode/src/main/java/org/project/Gui/MainSceneController.java
+++ GameCode/src/main/java/org/project/Gui/MainSceneController.java
@@ -1,20 +1,28 @@
 package org.project.Gui;
 
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.effect.BlurType;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
 import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import org.project.Model.Coordinates;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.IOException;
-import java.util.Random;
+import java.util.*;
 
 public class MainSceneController {
     private GuiUserInterface guiUserInterface;
@@ -22,6 +30,11 @@ public class MainSceneController {
 
     @FXML
     private GridPane GrigliaBoard;
+
+    private ArrayList<Rectangle> selectedBorders = new ArrayList<>();
+    private ArrayList<ImageView> selectedTiles = new ArrayList<>();
+
+    private ArrayList<Coordinates> coordinates = new ArrayList<>();
     private ImageView[] azureImage= {
             new ImageView(new Image(getClass().getResourceAsStream("/images/Trofei1.1.png"))),
             new ImageView(new Image(getClass().getResourceAsStream("/images/Trofei1.2.png"))),
@@ -90,24 +103,61 @@ public class MainSceneController {
                 tileImageView.setFitWidth(cellWidth);
                 tileImageView.setFitHeight(cellHeight);
                 GrigliaBoard.add(tileImageView, j, i);
+                int finalI = i;
+                int finalJ = j;
+                tileImageView.setOnMouseClicked(event -> selectTile(finalJ, finalI));
             }
         }
     }
-    public PropertyChangeListener getRefreshListener() {
-        return refreshlistener;
-    }
+    public void selectTile(int column, int row) {
+        ImageView tile = (ImageView) GrigliaBoard.getChildren().get(row * 9 + column);
 
-    PropertyChangeListener refreshlistener = new PropertyChangeListener() {
-        @Override
-        public void propertyChange(PropertyChangeEvent evt) {
-            Platform.runLater(() -> {
-                if("refresh".equals(evt.getPropertyName())){
-                    Random random=new Random();
-                }
-            });
+        if (selectedTiles.contains(tile)) {
+            // Tile is already selected, so deselect it
+            tile.setEffect(null);
+            selectedTiles.remove(tile);
+            GrigliaBoard.getChildren().remove(selectedBorders.get(0));
+            selectedBorders.remove(0);
+
+            return;
         }
-    };
 
+        if (selectedTiles.size() >= 3) {
+            // Maximum number of tiles already selected, remove the oldest selection
+            ImageView oldestTile = selectedTiles.get(0);
+            oldestTile.setEffect(null);
+
+            selectedTiles.remove(0);
+            GrigliaBoard.getChildren().remove(selectedBorders.get(0));
+            selectedBorders.remove(0);
+        }
+
+        // Create green rectangle border
+        selectedBorders.add(new Rectangle(tile.getFitWidth(), tile.getFitHeight()));
+        int size = selectedBorders.size();
+        Rectangle selectedBorder = selectedBorders.get(size - 1);
+        selectedBorder.setStroke(Color.GREEN);
+        selectedBorder.setStrokeWidth(2);
+        selectedBorder.setFill(Color.TRANSPARENT);
+
+        // Position the border over the tile
+        GridPane.setConstraints(selectedBorder, column, row);
+
+        // Add the border to the board
+        GrigliaBoard.getChildren().add(selectedBorder);
+
+        // Add the tile to the selected set
+        selectedTiles.add(tile);
+
+        // Apply visual effect to the selected tile
+        tile.setEffect(new DropShadow(BlurType.GAUSSIAN, Color.GREEN, 10, 0.5, 0, 0));
+    }
+    public void PickAction(ActionEvent actionEvent){
+        if(coordinates.size() > 0){
+            centralController.getGuiUserInterface().getClient().SendPickMessage(guiUserInterface.getNickname(), coordinates.size(), coordinates);
+        }
+        //centralController.getGuiUserInterface().getClient().SendPickMessage();
+    }
     public void setGuiUserInterface(GuiUserInterface guiUserInterface) {
         this.guiUserInterface = guiUserInterface;
     }

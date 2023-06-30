df72b3bc7d67d55af3c78c51c11e71e07ebe615d
mary <mariarosaria.cotrone@mail.polimi.it>
2023-06-30 10:48:52 +0200

    common goal 7 fixed

--- GameCode/src/main/java/org/project/Model/CommonGoals/CommonGoal_7.java
+++ GameCode/src/main/java/org/project/Model/CommonGoals/CommonGoal_7.java
@@ -19,26 +19,26 @@ public class CommonGoal_7 extends CommonGoal {
     }
     public boolean checkGoal(PlayerGrid playerGrid)
     {
-        int counter=0; //counter for row that verify the goal
+        int counter=0;
+        int checkPiena=0; //counter for row that verify the goal
         int i, j; //counters for playergrid
 
         for(i=0; i<6; i++)
         {
+            checkPiena=0;
             ArrayList<Integer> color= new ArrayList<Integer>();
             for(j=0; j<5; j++)
             {
-                if(!(playerGrid.getSpot(new Coordinates(i,j)).isOccupied()))
+                if((playerGrid.getSpot(new Coordinates(i,j)).isOccupied()))
                 {
-                    break;
-                }
-                else{
                     if(!(color.contains(playerGrid.getSpot(new Coordinates(i,j)).getTile().getColor().ordinal())))
                     {
                         color.add(playerGrid.getSpot(new Coordinates(i,j)).getTile().getColor().ordinal());
                     }
                 }
+                checkPiena++;
             }
-            if(color.size()<=3)
+            if(color.size()<=3 && checkPiena==5)
                 counter++;
         }
         if(counter>=4)
